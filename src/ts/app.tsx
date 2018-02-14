cb223eb48e5738619367f4e9be93eb5c5a050590
ponkore <ponkore@gmail.com>
2018-02-14 23:56:36 +0900

    still broken

--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -3,10 +3,3 @@ import * as ReactDOM from 'react-dom';
 import Hello from "./Hello";
 
 ReactDOM.render(<Hello content="hello world"/>, document.getElementById('app'));
-
-// namespace module1 {
-//     export class foo {
-//         constructor() {
-//         }
-//     }
-// }
