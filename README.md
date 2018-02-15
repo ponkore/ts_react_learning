928aba7dd3e4481c1da6494f6bd5e278e9576044
ponkore <ponkore@gmail.com>
2018-02-16 00:19:41 +0900

    add clean task

--- README.md
+++ README.md
@@ -31,7 +31,6 @@ $ gulp build:prod
 * [ ] index.html を dev/prod で分ける方法を考える
 * [ ] AP全体構成考えてみる(redux?) (container/page/component pattern?)
  - 参考：https://blog.mitsuruog.info/2017/08/look-back-react-typescript-after-half-year
-* [ ] gulp に clean タスク追加
 * [ ] css framework (bootstrap? material-ui? others?)
 * [ ] サンプルAPとして何を作るか考える(地図関連？)
 * [ ] テストコードを書く環境を作る(Mocha / Powerassert / ?)
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 const gulp = require('gulp');
+const mkdirp = require('mkdirp');
+const del = require('del');
 // const browserSync = require('browser-sync').create();
 const webpackStream = require('webpack-stream');
 const webpack = require('webpack');
 const webpackDevConfig = require('./webpack.config.dev.js');
 const webpackProdConfig = require('./webpack.config.prod.js');
+
+gulp.task('clean:dist', () => {
+    mkdirp('dist', (err) => {
+        if (err) console.error(err);
+    });
+    return del.sync(['dist/*']);
+});
 
 gulp.task('build:dev', () => {
     return webpackStream(webpackDevConfig, webpack)
